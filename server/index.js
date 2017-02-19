97b9cf5b7f160811d617437a703cea75eb71f977
tarnas14 <tarnas14@gmail.com>
2017-02-20 00:51:33 +0100

    fixed linting rules for indentation

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mognoose')
+const vouchersFactory = require('vouchers')
+
+const config = require('./config')
+const PORT = config.PORT || 3000
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoAddress)
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const vouchers = vouchersFactory(mongoose)
+app.post('/api/vouchers', vouchers.create)
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
