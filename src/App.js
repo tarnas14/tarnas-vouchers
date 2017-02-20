48e35e5bd0c9d44da8edb7be737092e954e7c8fa
tarnas14 <tarnas14@gmail.com>
2017-02-20 19:04:39 +0100

    displaying 3 cards with products from a hardcoded list, clicking BUY updates parent component state

--- src/App.js
+++ src/App.js
@@ -1,65 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import ProductGrid from './ProductGrid'
+
+import getProducts from './productsService'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.buy = this.buy.bind(this)
+    this.state = {
+      products: getProducts()
+    }
+  }
+
+  buy (productId) {
+    this.setState({
+      buying: productId
+    })
+  }
+
   render() {
+    const {buying, products} = this.state;
     return (
-    <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-color--grey-100">
-      <header className="mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-800">
-        <div className="mdl-layout__header-row">
-          <span className="mdl-layout-title">Tarnas voucher api test client</span>
-        </div>
-      </header>
-      <main className="mdl-layout__content">
-        <div className="mdl-grid">
-          <div className="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col">
-            <div className="mdl-card__title">
-              <h2 className="mdl-card__title-text">Welcome</h2>
-            </div>
-            <div className="mdl-card__supporting-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris sagittis pellentesque lacus eleifend lacinia...
-            </div>
-            <div className="mdl-card__actions mdl-card--border">
-              <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                Get Started
-              </a>
-            </div>
-            <div className="mdl-card__menu">
-              <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-                <i className="material-icons">share</i>
-              </button>
-            </div>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-color--grey-100">
+        <header className="mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-800">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">Tarnas voucher api test client</span>
           </div>
-          <div className="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col">
-            <div className="mdl-card__title">
-              <h2 className="mdl-card__title-text">Welcome</h2>
-            </div>
-            <div className="mdl-card__supporting-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris sagittis pellentesque lacus eleifend lacinia...
-            </div>
-            <div className="mdl-card__actions mdl-card--border">
-              <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                Get Started
-              </a>
-            </div>
-            <div className="mdl-card__menu">
-              <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-                <i className="material-icons">share</i>
-              </button>
-            </div>
+        </header>
+        <main className="mdl-layout__content">
+          {buying ? <div>hello, buying product {buying}</div> : null}
+          <ProductGrid products={products} buy={this.buy}></ProductGrid>
+        </main>
+        <footer className="mdl-mini-footer">
+          <div className="mdl-mini-footer__left-section">
+            2017 by <a href="https://github.com/tarnas14" target="about:blank">tarnas</a>
           </div>
-        </div>
-      </main>
-      <footer className="mdl-mini-footer">
-        <div className="mdl-mini-footer__left-section">
-          2017 by <a href="https://github.com/tarnas14" target="about:blank">tarnas</a>
-        </div>
-      </footer>
-    </div>
-    );
+        </footer>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/ProductGrid.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ProductGrid = props => (
+  <div className="mdl-grid">
+    {props.products.map(product => (
+      <div className="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col">
+        <div className="mdl-card__title">
+          <h2 className="mdl-card__title-text">{product.name}</h2>
+        </div>
+        <div className="mdl-card__supporting-text">{product.description}</div>
+        <div className="mdl-card__actions mdl-card--border">
+          <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onClick={props.buy.bind(null, product.id)}>
+            BUY
+          </a>
+        </div>
+        <div className="mdl-card__menu">
+          <span className="mdl-chip">
+            <span className="mdl-chip__text">${product.price}</span>
+          </span>
+        </div>
+      </div>
+    ))}
+  </div>
+)
+
+export default ProductGrid
